f77e231df0b63973992dc2b76d3fc109fe2bfbfa
Teerasej <teerasej@gmail.com>
2021-03-06 16:39:39 +0700

    prepare for starter

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _currentLocationText = "...";
+
   List<LocationData> locationDataList = [];
   Location keepUpdateLocation;
 
